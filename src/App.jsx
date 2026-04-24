d2004053a690d0ce8f317d02851bf0a01d64346e
angelapg14 <angelpacheco11@gmail.com>
2026-04-24 10:43:59 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export default function App(){return <div style={{padding:20,color:'white',background:'#020617',minHeight:'100vh'}}>Paste your current MLB Sharp Board canvas code into src/App.jsx to replace this starter file.</div>}
